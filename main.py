13d1d65b25c0acb445fead383c373310e91b5787
lby0615 <bjl5716@psu.edu>
2020-09-10 17:29:20 +0000

    add two def

--- main.py
+++ main.py
@@ -4,34 +4,30 @@
 # Collaborator:
 # Section: 4
 # Breakout: 12Í
-def getLetterGrade():
-  grade = float(input("Enter your CMPSC 131 grade: "))
-
-  if grade <=100.0 and grade >= 93.0:
-    print ("Your letter grade for CMPSC 131 is A+.")
-  elif grade < 93.0 and grade >= 90.0:
-    print ("Your letter grade for CMPSC 131 is A.")
-  elif grade < 90.0 and grade >= 87.0:
-    print ("Your letter grade for CMPSC 131 is B+.")
-  elif grade < 87.0 and grade >= 83.0:
-    print ("Your letter grade for CMPSC 131 is B.")
-  elif grade < 83.0 and grade >= 80.0:
-    print ("Your letter grade for CMPSC 131 is B-.")
-  elif grade < 80.0 and grade >= 77.0:
-    print ("Your letter grade for CMPSC 131 is C+.")
-  elif grade < 77.0 and grade >= 70.0:
-    print ("Your letter grade for CMPSC 131 is C.")
-  elif grade < 70.0 and grade >= 60.0:
-    print ("Your letter grade for CMPSC 131 is D.")
-  else:
-    print ("Your letter grade for CMPSC 131 is F.")
+def getLetterGrade(grade):
   
+  if grade <=100.0 and grade >= 93.0:
+    return ("Your letter grade for CMPSC 131 is A.")
+  elif grade < 93.0 and grade >= 90.0:
+    return ("Your letter grade for CMPSC 131 is A-.")
+  elif grade < 90.0 and grade >= 87.0:
+    return ("Your letter grade for CMPSC 131 is B+.")
+  elif grade < 87.0 and grade >= 83.0:
+    return ("Your letter grade for CMPSC 131 is B.")
+  elif grade < 83.0 and grade >= 80.0:
+    return ("Your letter grade for CMPSC 131 is B-.")
+  elif grade < 80.0 and grade >= 77.0:
+    return ("Your letter grade for CMPSC 131 is C+.")
+  elif grade < 77.0 and grade >= 70.0:
+    return ("Your letter grade for CMPSC 131 is C.")
+  elif grade < 70.0 and grade >= 60.0:
+    return ("Your letter grade for CMPSC 131 is D.")
+  else:
+    return ("Your letter grade for CMPSC 131 is F.")
 
+def run():
+  grade = float(input("Enter your CMPSC 131 grade: "))
+  print (getLetterGrade(grade))
+  
 if __name__ == "__main__":
-  getLetterGrade()
-
-
-
-
-
-
+  run()
